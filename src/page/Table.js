09e1a650d64bf3a0495adc05dcba54a8e0c86968
naziiriah <nazistry@gmail.com>
2022-07-12 14:00:44 +0100

    Design update

--- src/page/Table.js
+++ src/page/Table.js
@@ -1,11 +1,12 @@
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { HomeLightBTN } from "../components/Buttons"
-import { BillHeader } from "../components/Header"
 import { removeFromCart } from "../redux"
-import EmptyImage from "../images/Empty-Menu.png"
-import {TiTimes} from  "react-icons/ti"
+import EmptyImage from "../images/DishNew.png"
+import CutleryImage from "../images/Cultery.png"
+import {HiOutlineTrash} from  "react-icons/hi"
 import { Icon } from "@chakra-ui/react"
+import {IoIosArrowBack} from "react-icons/io"
 
 
 const Table = () => {
@@ -20,10 +21,17 @@ const Table = () => {
     
     return(
         <>
-            <BillHeader title={'table'}/>
+             <header className="table-design">
+                <div className="container bill-header header menu_pages">
+                    <Icon as={IoIosArrowBack} ml="-.5rem" color="#000"
+                            onClick={() => navigate('/menu')}/>
+                    <>table</>
+                    <div></div>
+                </div>
+            </header>
             {tableList && tableList.length > 0 ?
-                <main className="container table-design menu_pages">
-                    <section className="bill__header">
+                <main className="table-design menu_pages">
+                    <section className="container  bill__header">
                         <nav className="bill__flex-title">
                             <h2 className=" bill__title bill__title_1">qty</h2>
                             <h2 className=" bill__title start bill__title_2">item</h2>
@@ -34,7 +42,7 @@ const Table = () => {
                                         <div className="bill__items" key={state.id}>
                                             <h3 className="bill__item bill__title_1">{state.quantity}</h3>
                                             <h3 className="bill__item bill__title_2">{state.name}</h3>
-                                            <h3 className="bill__cancel bill__title_3" onClick={() => removeItem(state.id)}><Icon as={TiTimes} fontSize={"22px"}  mt={'-0.2rem'}/></h3>
+                                            <h3 className="bill__cancel bill__title_3" onClick={() => removeItem(state.id)}><Icon as={HiOutlineTrash} fontSize={"22px"}  mt={'-0.2rem'} color={'red'} opacity='0.8'/></h3>
                                         </div>
                                 )) 
                             }
@@ -44,19 +52,17 @@ const Table = () => {
                                 change order
                             </button>
                             <button className="btn__custom btn--dark" onClick={() => navigate('/bill')}>
-                                confirm order
+                                Send to waiter
                             </button>
                         </div>
                     </section>
-                    <aside>
-
-                    </aside>
                 </main>
                 :
-                <main className="container table-design menu_pages">
-                <section>
-                    <div className="bill__empty"> 
-                        <img src={EmptyImage} alt="Wine and Dine" className="table__Image"/>
+                <main className="table-design menu_pages">
+                <section className="container">
+                    <div className="table__empty"> 
+                        <img src={EmptyImage} alt="Wine and Dine" className="table__Image1"/>
+                        <img src={CutleryImage} alt="Wine and Dine" className="table__Image2"/>
 
                         <h2 className="bill__emptyDirective">You haven't  ordered anything off the menu yet. <br/>
                             View the menu to select items.
