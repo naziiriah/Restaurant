df57b21f11c7656ec33ed663a269bda6a41c322e
naziiriah <nazistry@gmail.com>
2022-07-24 19:25:43 +0100

    Final+page gif  correction

--- src/page/Final.js
+++ src/page/Final.js
@@ -1,7 +1,7 @@
 import { useEffect, } from "react"
 import { useNavigate } from "react-router-dom"
 import finalImage from "../images/success.png"
-import gif from "../images/200 (2).webp"
+import gif from "../images/he.gif"
 const Final = () => {
     
     const navigate = useNavigate();
